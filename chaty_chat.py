5624fbc94461f19eb57a0f26254e9918d986aa50
mrbertp <aps795@gmail.com>
2020-05-16 03:56:23 +0200

    coded focus on message box

--- chaty_chat.py
+++ chaty_chat.py
@@ -6,7 +6,25 @@ root = Tk()
 root.title("Chaty Chat")
 root.geometry(f"{int(350)}x{int(350*PHI)}")
 
-message_box = Entry(root, text="Aquí tu mensaje")
-message_box.pack(padx=5, pady=5, side=BOTTOM, fill=BOTH)
+
+def msgbox_focus(event):
+    print(".!", event.widget)
+    if str(event.widget) == ".!entry":
+        print("pipo")
+        message_box.delete(0, END)
+    elif str(event.widget) != ".!entry":
+        message_box.delete(0, END)
+        message_box.insert(0, "Aquí tu mensaje")
+    return None
+
+
+message_box = Entry(root)
+message_box.insert(0, "Aquí tu mensaje")
+message_box.pack(padx=5,
+                 pady=5,
+                 side=BOTTOM,
+                 fill=BOTH)
+root.bind("<Button-1>", msgbox_focus)
+
 
 root.mainloop()
