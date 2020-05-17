fb3293be06f555f946842431087b02b4e9cb5301
mrbertp <aps795@gmail.com>
2020-05-17 02:00:51 +0200

    improved callback on message box

--- chaty_chat.py
+++ chaty_chat.py
@@ -4,27 +4,41 @@ PHI = (1+5**0.5)/2
 
 root = Tk()
 root.title("Chaty Chat")
-root.geometry(f"{int(350)}x{int(350*PHI)}")
+WIDTH = int(350)
+HEIGHT = int(350*PHI)
+root.geometry(f"{WIDTH}x{HEIGHT}")
 
 
 def msgbox_focus(event):
-    print(".!", event.widget)
-    if str(event.widget) == ".!entry":
-        print("pipo")
-        message_box.delete(0, END)
-    elif str(event.widget) != ".!entry":
-        message_box.delete(0, END)
-        message_box.insert(0, "Aquí tu mensaje")
+    if event.widget == root.nametowidget(msg_box):
+        msg_box.delete(0, END)
+    elif event.widget != root.nametowidget(msg_box):
+        msg_box.delete(0, END)
+        msg_box.insert(0, "Enter to send message")
     return None
 
 
-message_box = Entry(root)
-message_box.insert(0, "Aquí tu mensaje")
-message_box.pack(padx=5,
-                 pady=5,
-                 side=BOTTOM,
-                 fill=BOTH)
+buttons_frame = LabelFrame(root)
+buttons_frame.grid(row=0, column=0, sticky=NSEW)
+button = Button(buttons_frame, text="Adios")
+button.pack(fill=BOTH)
+
+chat_frame = LabelFrame(root)
+chat_frame.grid(row=1, column=0, sticky=NSEW)
+chat_box = Label(chat_frame, text="hola")
+chat_box.pack(fill=BOTH)
+
+msgbox_frame = LabelFrame(root)
+msgbox_frame.grid(row=2, column=0, sticky=NSEW)
+msg_box = Entry(msgbox_frame)
+msg_box.pack(side=BOTTOM, fill=BOTH)
+msg_box.insert(0, "Enter to send message")
 root.bind("<Button-1>", msgbox_focus)
+
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_rowconfigure(2, weight=1)
 
 
 root.mainloop()
